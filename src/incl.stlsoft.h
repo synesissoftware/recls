8c017d7b45d87a8ba71773c631618d0822cce124
Matt Wilson <matthew@synesis.com.au>
2020-12-21 19:39:56 +1100

    ~ STLSoft compatibility

--- src/incl.stlsoft.h
+++ src/incl.stlsoft.h
@@ -4,11 +4,12 @@
  * Purpose:     #includes the STLSoft root header and verifies the version.
  *
  * Created:     8th November 2007
- * Updated:     29th January 2017
+ * Updated:     21st December 2020
  *
  * Home:        http://recls.org/
  *
- * Copyright (c) 2003-2017, Matthew Wilson and Synesis Software
+ * Copyright (c) 2019-2020, Matthew Wilson and Synesis Information Systems
+ * Copyright (c) 2003-2019, Matthew Wilson and Synesis Software
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without 
@@ -31,17 +32,33 @@
 # undef RECLS_STLSOFT_1_12_OR_LATER
 #endif /* RECLS_STLSOFT_1_12_OR_LATER */
 
-#include <stlsoft/stlsoft.h>    /* If the compiler cannot find this, you are not using STLSoft 1.9.1 or later, as required. */
+#include <stlsoft/stlsoft.h>    /* If the compiler cannot find this, you are not using STLSoft 1.10.1 or later, as required. */
 #if 0
-#elif defined(STLSOFT_VER) && \
-      STLSOFT_VER >= 0x010c0000
-# define RECLS_STLSOFT_1_12_OR_LATER
-#elif defined(_STLSOFT_VER) && \
-      _STLSOFT_VER >= 0x010a0181
-# define RECLS_STLSOFT_1_10_B01_OR_LATER
-#elif !defined(_STLSOFT_VER) || \
-   _STLSOFT_VER < 0x010981ff
-# error Requires STLSoft 1.9.129, or later. (www.stlsoft.org)
+#elif defined(STLSOFT_VER)
+
+# if 0
+# elif STLSOFT_VER >= 0x010c0000
+#  define RECLS_STLSOFT_1_12_OR_LATER
+# elif STLSOFT_VER >= 0x010b0000
+#  define RECLS_STLSOFT_1_11_OR_LATER
+# elif STLSOFT_VER >= 0x010a019a
+#  define RECLS_STLSOFT_1_10_B26_OR_LATER
+# else
+#  error use of STLSoft 1.10 requires Version 1.10.1 beta 26 or later
+# endif
+
+#elif defined(_STLSOFT_VER)
+
+# if 0
+# elif _STLSOFT_VER >= 0x010c0000
+#  define RECLS_STLSOFT_1_12_OR_LATER
+# elif _STLSOFT_VER >= 0x010b0000
+#  define RECLS_STLSOFT_1_11_OR_LATER
+# elif _STLSOFT_VER >= 0x010a019a
+#  define RECLS_STLSOFT_1_10_B26_OR_LATER
+# else
+#  error use of STLSoft 1.10 requires Version 1.10.1 beta 26 or later
+# endif
 #endif /* STLSoft version */
 
 /* /////////////////////////////////////////////////////////////////////////
@@ -50,7 +67,8 @@
 
 #if defined(__cplusplus) && \
     defined(_STLSOFT_NO_NAMESPACE)
-# error recls 1.9+ is not compatible with STLSoft namespace suppression
+
+# error recls 1.10+ is not compatible with STLSoft namespace suppression
 #endif /* _STLSOFT_NO_NAMESPACE */
 
 /* ////////////////////////////////////////////////////////////////////// */
@@ -58,3 +76,4 @@
 #endif /* !RECLS_INCL_SRC_H_INCL_STLSOFT */
 
 /* ///////////////////////////// end of file //////////////////////////// */
+
