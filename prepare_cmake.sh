a243386a600d7119e8fef2b6eee1373681776f90
Matt Wilson <152443343+mwsis@users.noreply.github.com>
2025-04-30 21:55:09 +1000

    '--no-shwild' (#50)
    * 1.10.1-beta6
    
    * + TODOs
    
    * **prepare_cmake.sh** / **CMakeLists.txt** : + added '--no-shwild' / `CMAKE_NO_SHWILD`
    
    ---------
    
    Co-authored-by: Matt Wilson <matthew@synesis.com.au>

--- prepare_cmake.sh
+++ prepare_cmake.sh
@@ -11,6 +11,7 @@ ExamplesDisabled=0
 MinGW=0
 NO_b64=0
 NO_Pantheios=0
+NO_shwild=0
 RunMake=0
 STLSoftDirGiven=
 TestingDisabled=0
@@ -50,6 +51,10 @@ while [[ $# -gt 0 ]]; do
     --no-pantheios)
 
       NO_Pantheios=1
+      ;;
+    --no-shwild)
+
+      NO_shwild=1
       ;;
     -m|--run-make)
 
@@ -101,6 +106,9 @@ Flags/options:
     --no-pantheios
         prevents recognising Pantheios library
 
+    --no-shwild
+        prevents recognising shwild library
+
     -m
     --run-make
         executes make after a successful running of CMake
@@ -145,6 +153,7 @@ echo "Executing CMake (in ${CMakeDir})"
 if [ $ExamplesDisabled -eq 0 ]; then CMakeBuildExamplesFlag="ON" ; else CMakeBuildExamplesFlag="OFF" ; fi
 if [ $NO_b64 -eq 0 ]; then CMakeNoB64="OFF" ; else CMakeNoB64="ON" ; fi
 if [ $NO_Pantheios -eq 0 ]; then CMakeNoPantheios="OFF" ; else CMakeNoPantheios="ON" ; fi
+if [ $NO_shwild -eq 0 ]; then CMakeNoShwild="OFF" ; else CMakeNoShwild="ON" ; fi
 if [ -z $STLSoftDirGiven ]; then CMakeSTLSoftVariable="" ; else CMakeSTLSoftVariable="-DSTLSOFT=$STLSoftDirGiven/" ; fi
 if [ $TestingDisabled -eq 0 ]; then CMakeBuildTestingFlag="ON" ; else CMakeBuildTestingFlag="OFF" ; fi
 if [ $VerboseMakefile -eq 0 ]; then CMakeVerboseMakefileFlag="OFF" ; else CMakeVerboseMakefileFlag="ON" ; fi
@@ -158,6 +167,7 @@ if [ $MinGW -ne 0 ]; then
     -DCMAKE_BUILD_TYPE=$Configuration \
     -DCMAKE_NO_B64:BOOL=$CMakeNoB64 \
     -DCMAKE_NO_PANTHEIOS:BOOL=$CMakeNoPantheios \
+    -DCMAKE_NO_SHWILD:BOOL=$CMakeNoShwild \
     -G "MinGW Makefiles" \
     -S $Dir \
     -B $CMakeDir \
@@ -171,6 +181,7 @@ else
     -DCMAKE_BUILD_TYPE=$Configuration \
     -DCMAKE_NO_B64:BOOL=$CMakeNoB64 \
     -DCMAKE_NO_PANTHEIOS:BOOL=$CMakeNoPantheios \
+    -DCMAKE_NO_SHWILD:BOOL=$CMakeNoShwild \
     -DCMAKE_VERBOSE_MAKEFILE:BOOL=$CMakeVerboseMakefileFlag \
     -S $Dir \
     -B $CMakeDir \
