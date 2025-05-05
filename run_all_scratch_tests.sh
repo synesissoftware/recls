849636db274ad4e734e50e08b96c3a1eabc2bb8a
Matt Wilson <matthew.wilson.synesis@gmail.com>
2025-05-05 11:23:56 +1000

    Merged 'b7' into 'UNIXem'
    Squashed commit of the following:
    
    commit cc25bb7288e64aea35cdc6b61236616cefe3c710
    Author: Matt Wilson <matthew.wilson.synesis@gmail.com>
    Date:   Mon May 5 11:06:07 2025 +1000
    
        CMake scripts : + now recognises `SIS_CMAKE_MAKE_COMMAND` in addition to `SIS_CMAKE_COMMAND`
    
    commit 661b0cff4b666ac3b6ecfddbf398f0c2281b1ec7
    Merge: 2c14367 9d3c8be
    Author: Matt Wilson <matthew.wilson.synesis@gmail.com>
    Date:   Mon May 5 09:28:05 2025 +1000
    
        Merge branch 'dev-1.10' into 'b7'
    
    commit 2c14367db64920e9bc9221d454d8a8ff19ed2ca6
    Author: Matt Wilson <matthew.wilson.synesis@gmail.com>
    Date:   Mon May 5 06:46:06 2025 +1000
    
        Merged 'no-pan' into 'dev-1.10'
    
        Squashed commit of the following:
    
        commit c8a798a303e437e818443ee4b88d0e042ce5f1aa
        Author: Matt Wilson <matthew.wilson.synesis@gmail.com>
        Date:   Sun May 4 14:46:46 2025 +1000
    
            **prepare_cmake.sh** : + flag '--no-pantheios' now also suppresses discovery of b64 package
    
        commit 1e4e8c13520543af3c287f3fb7c4a6c8ae438f34
        Author: Matt Wilson <matthew.wilson.synesis@gmail.com>
        Date:   Sun May 4 14:45:48 2025 +1000
    
            **prepare_cmake.sh** : + added alias '--no-pan' for flag '--no-pantheios'
    
        commit 005803cdb3e94b7adfddca0e3d9ea82b2a902e68
        Author: Matt Wilson <matthew.wilson.synesis@gmail.com>
        Date:   Sun May 4 14:44:52 2025 +1000
    
            **prepare_cmake.sh** : ~ minor improvements in driver script '--help' flag descriptions
    
        commit 4888e376a243850ce6777ec20a5cb6177e504aaa
        Merge: 97dbac1 3583581
        Author: Matt Wilson <matthew.wilson.synesis@gmail.com>
        Date:   Sun May 4 14:26:46 2025 +1000
    
            Merge branch 'dev-1.10' into 'no-pan'
    
        commit 97dbac1b59f47313ba3ebb3e523de191adf46239
        Author: Matt Wilson <matthew.wilson.synesis@gmail.com>
        Date:   Sat May 3 17:29:19 2025 +1000
    
            ~ no-Pantheios compatibility

--- run_all_scratch_tests.sh
+++ run_all_scratch_tests.sh
@@ -4,7 +4,7 @@ ScriptPath=$0
 Dir=$(cd $(dirname "$ScriptPath"); pwd)
 Basename=$(basename "$ScriptPath")
 CMakeDir=${SIS_CMAKE_BUILD_DIR:-$Dir/_build}
-MakeCmd=${SIS_CMAKE_COMMAND:-make}
+MakeCmd=${SIS_CMAKE_MAKE_COMMAND:-${SIS_CMAKE_COMMAND:-make}}
 
 ListOnly=0
 RunMake=1
