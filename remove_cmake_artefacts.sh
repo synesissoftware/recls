04e5fd784edc8007aae3c9c2b7f693601af59509
Matt Wilson <matthew.wilson.synesis@gmail.com>
2026-07-03 14:13:03 +1000

    chore: helper scripts now implemented in terms of **/.sis/script_info_lines.txt**

--- remove_cmake_artefacts.sh
+++ remove_cmake_artefacts.sh
@@ -56,10 +56,8 @@ while [[ $# -gt 0 ]]; do
   case $1 in
     --help)
 
+      [ -f "$Dir/.sis/script_info_lines.txt" ] && cat "$Dir/.sis/script_info_lines.txt"
       cat << EOF
-recls is a platform-independent recursive file-system search library
-Copyright (c) 2019-2024, Matthew Wilson and Synesis Information Systems
-Copyright (c) 2003-2019, Matthew Wilson and Synesis Software
 Removes all known CMake artefacts
 
 $ScriptPath [ ... flags/options ... ]
